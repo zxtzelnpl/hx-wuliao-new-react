83d75f28efc104d6a556e2a3c2b0103487fbc9bd
zxt_z <zxt_zel_npl@163.com>
2018-06-25 21:40:41 +0800

    messageBox finished

--- src/components/Chat/views/MessageBox.js
+++ src/components/Chat/views/MessageBox.js
@@ -10,9 +10,17 @@ class MessageBox extends Component {
     super(props)
     this.renderItems = this.renderItems.bind(this);
     this.box = React.createRef();
+    this.wrap = React.createRef();
+    this.loadBefore = React.createRef();
+    this.loadMore = React.createRef();
   }
 
-  componentDidMount() {
+  initialJRoll(){
+
+    let loadBeforeH = this.loadBefore.current.clientHeight;
+    let loadMoreH = this.loadMore.current.clientHeight;
+    let boxH = this.box.current.clientHeight;
+    let height = this.wrap.current.clientHeight;
 
     this.box.current.addEventListener('mousewheel',this.onWheelHandle);
 
@@ -20,13 +28,47 @@ class MessageBox extends Component {
       scrollX:false,
       scrollY:true,
       scrollBarY: true,
-      scrollBarFade:true
+      scrollBarFade:true,
+      minY:loadBeforeH,
+      maxY:-(height+loadMoreH-boxH)
     });
+    this.jroll.on('scrollEnd',()=>{
+      if(this.jroll.y>=loadBeforeH){
+        console.log('loadBefore');
+        setTimeout(()=>{
+          console.log('loaded')
+        },1000)
+      }
+      else if(this.jroll.y>0){
+        this.jroll.scrollTo(0,0,300);
+      }
+      else if(this.jroll.y<=-(height+loadMoreH - boxH)){
+        console.log('loadMore');
+        setTimeout(()=>{
+          console.log('loaded');
+        },1000)
+      }
+      else if(this.jroll.y<-(height-boxH)){
+        this.jroll.scrollTo(0,-(height - boxH),300);
+      }
+    })
+  }
+
+  destoryJRoll(){
+    this.box.current.removeEventListener('mousewheel',this.onWheelHandle);
+    this.jroll.destroy();
+  }
+
+  componentDidMount() {
+    this.initialJRoll()
   }
 
   componentWillUnmount(){
-    this.box.current.removeEventListener('mousewheel',this.onWheelHandle);
-    this.jroll.destroy();
+    this.destoryJRoll();
+  }
+
+  componentDidUpdate(){
+
   }
 
   onWheelHandle(e){
@@ -47,8 +89,14 @@ class MessageBox extends Component {
     return (
       <div className="messageBoxWrap" >
         <div className="messageBox" ref={this.box}>
-          <div className="messageBoxInnerWrap">
+          <div className="messageBoxInnerWrap" ref={this.wrap}>
+            <div className="scrollDownToLoadBefore" ref={this.loadBefore}>
+              LoadBefore
+            </div>
             {this.renderItems()}
+            <div className="scrollUpToLoadMore" ref={this.loadMore}>
+              LoadMore
+            </div>
           </div>
         </div>
       </div>
