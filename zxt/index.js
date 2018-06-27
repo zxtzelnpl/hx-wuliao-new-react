a6da75683fbb7591e0b73b6a5891e5b44e33f548
zxt_z <zxt_zel_npl@163.com>
2018-06-27 23:25:24 +0800

    two changes yeah

---
+++ zxt/index.js
@@ -0,0 +1,15 @@
+function readLinesSync(file) {
+  return {
+    [Symbol.iterator]() {
+      return {
+        next() {
+          return { done: false };
+        },
+        return() {
+          file.close();
+          return { done: true };
+        }
+      };
+    },
+  };
+}
