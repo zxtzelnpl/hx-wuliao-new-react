17f2fecde484fd4e95011400be0fe0803308e345
was <zxt_zel_npl@163.com>
2018-08-01 13:21:43 +0800

    fix videoPage replay

--- src/components/CRMVideo/views/ReplayVideoItem.js
+++ src/components/CRMVideo/views/ReplayVideoItem.js
@@ -9,10 +9,10 @@ class ReplayVideoItem extends Component {
     const {title,url} = this.props;
 
     return (
-      <li className="replayVideoItem">
-        <a target={"_blank"} href={url}>
+      <li className="replay-video-item">
+        <a className="replay-video-item-a" target={"_blank"} href={url}>
           <img className="img" src={img} />
-          <p className="subTitle">{title}</p>
+          <p className="sub-title">{title}</p>
         </a>
       </li>
     )
