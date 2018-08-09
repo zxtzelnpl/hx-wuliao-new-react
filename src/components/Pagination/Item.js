8966e0b7a92b752dd222d09e476458f1d1c6c0f5
was <zxt_zel_npl@163.com>
2018-08-09 10:45:41 +0800

    fix time and date

--- src/components/Pagination/Item.js
+++ src/components/Pagination/Item.js
@@ -10,11 +10,12 @@ import PageImgContent from 'components/PageContent/PageImgContent';
 import {pdfReg, officeReg, imgReg} from "utils/tools";
 
 const FORMAT = 'YYYY-MM-DD hh:mm';
-function getTime(a,b){
-  if(a!==undefined){
+
+function getTime(a, b) {
+  if (a !== undefined) {
     return moment.unix(a).format(FORMAT);
   }
-  else if(b!==undefined){
+  else if (b !== undefined) {
     return moment.unix(b).format(FORMAT);
   }
 }
@@ -22,58 +23,56 @@ function getTime(a,b){
 class Item extends Component {
 
   state = {
-    show:false
+    show: false
   }
 
   renderContent = () => {
-    let {content,filepath,timestamp} = this.props
+    let {content, filepath, timestamp} = this.props
     let {show} = this.state;
 
-    if(!show){
+    if (!show) {
       return null;
     }
 
-    if (content!==''&&content!==null) {
+    if (content !== '' && content !== null) {
       return <PageHtmlContent htmlDom={content} filepath={filepath}/>
     }
     if (filepath) {
-      if(pdfReg.test(filepath)){
+      if (pdfReg.test(filepath)) {
         return <PagePDFContent filepath={filepath} timestamp={timestamp}/>
       }
 
-      if(imgReg.test(filepath)){
+      if (imgReg.test(filepath)) {
         return <PageImgContent filepath={filepath}/>
       }
 
       let match = filepath.match(officeReg);
 
-      if(match){
+      if (match) {
         return <PageOfficeContent filepath={filepath} type={match[1]}/>
       }
     }
   }
 
-  handleClick = ()=>{
-    this.setState(({show})=>({show:!show}))
+  handleClick = () => {
+    this.setState(({show}) => ({show: !show}))
   }
 
   render() {
-    const {title,author,timestamp,new_timestamp} = this.props;
-    const header = classNames('page-item-header',{
-      show:this.state.show
+    const {title, author, timestamp, new_timestamp} = this.props;
+    const header = classNames('page-item-header', {
+      show: this.state.show
     })
-    const time = getTime(timestamp,new_timestamp);
+    const time = getTime(timestamp, new_timestamp);
     return (
       <li className="page-item">
         <div className={header} onClick={this.handleClick}>
           <div className="page-item-header-title">
             {title}
-            <span className="page-item-header-title-sub">
-              {author&&`------${author}`}
-            </span>
-            <span className="page-item-header-title-sub">
-              {time&&`------${time}`}
-            </span>
+          </div>
+          <div className="page-item-header-sub">
+            {author && <span className="page-item-header-sub-author">上传用户：{author}</span>}
+            {time && <span className="page-item-header-sub-time">上传时间：{time}</span>}
           </div>
         </div>
         {this.renderContent()}
